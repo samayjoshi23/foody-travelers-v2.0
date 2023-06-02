bb83bd1693df400a5e3d7fe0e435a04ff9afa32e
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2023-06-03 01:29:17 +0530

    Moved Other Schema To respective controllers

--- Controllers/bookingControllers.js
+++ Controllers/bookingControllers.js
@@ -1,9 +1,106 @@
-// Middleware import
+// Middleware and Lib import
 const AppError = require('../utils/AppError');
 const mongoose = require('mongoose');
 
-// Schema
-const Ticket = require('../Models/TicketSchema');
+// ---------------- DefaultCode --------------------
+// const State = require('../Models/StateSchema');
+// const Ticket = require('../Models/TicketSchema');
+
+
+// Added - Schema
+const ticketSchema = new mongoose.Schema({
+    user_Id: String,
+    user_Name: String,
+    state_Name: {
+        type : String,
+        required: true
+    },
+    tour_Source: {
+        type: String,
+        required: true
+    },
+    base_Fare: {
+        type: Number,
+        required: true
+    },
+    date_of_Booking: {
+        type: String,
+        required: true
+    },
+    date_of_Journey:{
+        type: String,
+        required: true
+    },
+    no_of_persons: {
+        type: Number,
+        required: true
+    },
+    passenger_details: [
+        {
+            name: {
+                type:String,
+                required: true
+            },
+            age: {
+                type:Number,
+                required: true
+            },
+            aadharId: {
+                type: Number,
+                required: true
+            },
+            gender: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    food_Opted: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            Price: {
+                type: Number,
+                required: true
+            },
+            Qty: {
+                type: Number,
+                required: true
+            },
+            TotalCost: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    food_Fare:{
+        type: Number,
+        required: true
+    },
+    vehicle_Type: {
+        type: String,
+        required: true
+    },
+    vehicle_Fare:{
+        type: Number,
+        required: true
+    },
+    pickup_State: {
+        type: String,
+        required: true
+    },
+    pickup_City: {
+        type: String,
+        required: true
+    },
+    grand_Fare: {
+        type: Number,
+        required: true
+    }
+});
+
 const stateSchema = new mongoose.Schema({
     state_name : {
         type: String,
@@ -53,6 +150,7 @@ const stateSchema = new mongoose.Schema({
 });
 
 const State = mongoose.model('State', stateSchema);
+const Ticket = mongoose.model('Ticket', ticketSchema);
 
 
 module.exports.getStates = async (req,res,next)=> {
