cd8e239b6a0318aed1848950155b485d5eb7c81a
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2023-06-03 01:15:30 +0530

    Moved StateSchema To bookingController

--- Controllers/bookingControllers.js
+++ Controllers/bookingControllers.js
@@ -1,9 +1,59 @@
-// Schema
-const State = require('../Models/StateSchema');
-const Ticket = require('../Models/TicketSchema');
-// const User = require('../Models/UserSchema');
-// middleware import
+// Middleware import
 const AppError = require('../utils/AppError');
+const mongoose = require('mongoose');
+
+// Schema
+const Ticket = require('../Models/TicketSchema');
+const stateSchema = new mongoose.Schema({
+    state_name : {
+        type: String,
+        required: true
+    },
+    state_home_cover: {
+        type: String,
+        required: true
+    },
+    cities: [{
+        city_name: String,
+        places: [{
+            img_url: String,
+            place_name: String,
+        }],
+        city_hotel: {
+            hotel_name: String,
+            hotel_img:  String
+        },
+        city_restaurant: {
+            restaurant_name: String,
+            restaurant_img: String
+        },
+    }],
+    state_food: {
+        type: [{
+            f_name: String,
+            food_img: String,
+            price: Number,
+            food_type: String,
+            dot_color: String
+        }],
+        required: true
+    },
+    base_fare: {
+        type: Number,
+        required: true
+    },
+    duration:{ 
+        type: String,
+        required: true
+    },
+    tour_source: {
+        type: String,
+        required: true
+    }
+});
+
+const State = mongoose.model('State', stateSchema);
+
 
 module.exports.getStates = async (req,res,next)=> {
     let user = req.user;
